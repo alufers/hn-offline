a802b688f99a19b5dad84c303c57eeec64fb03e3
alufers <alufers@wp.pl>
2019-07-08 09:44:45 +0200

    Remove dummy intem entries and add a real loading hook

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,28 +1,49 @@
 import { h } from "preact";
-import "./style.less";
-import Navbar from "../Navbar";
+import { useEffect, useState } from "preact/hooks";
+import MessageType from "../common/MessageType.enum";
 import Footer from "../Footer";
 import ItemHead from "../ItemHead";
 import LoadingPlaceholder from "../ItemHead/LoadingPlaceholder";
+import Navbar from "../Navbar";
+import useServiceWorkerClient from "../ServiceWorkerClient/useServiceWorkerClient";
+import Item from "../types/Item";
+import "./style.less";
+
 h;
 
 export default () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [items, setItems] = useState(null);
+  const serviceWorkerClient = useServiceWorkerClient();
+  useEffect(() => {
+    const loadItems = async () => {
+      setIsLoading(true);
+      try {
+        setItems(
+          await serviceWorkerClient.request<Item[]>(MessageType.GetItems, {})
+        );
+      } finally {
+        setIsLoading(false); 
+      }
+    };
+    loadItems();
+  }, []);
+
+  let content = null;
+  if (isLoading) {
+    content = [];
+    for (let i = 0; i < 10; i++) {
+      content.push(<LoadingPlaceholder key={i} />);
+    }
+  } else if (!!items) {
+    content = items.map(item => <ItemHead item={item} key={item.id} />);
+  }
+
   return (
     <div>
       <Navbar />
       <div styleName="container">
-        <ItemHead
-          item={
-            {
-              title:
-                "Why did moving the mouse cursor cause Windows 95 to run more quickly?",
-              descendants: 23,
-              by: "folli",
-              score: 153
-            } as any
-          }
-        />
-        <LoadingPlaceholder />
+        {content}
         <Footer />
       </div>
     </div>

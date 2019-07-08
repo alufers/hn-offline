31a91a3d6deaa225fd658c5af1b62135a2402f9f
alufers <alufers@wp.pl>
2019-07-08 09:44:22 +0200

    Remove debugging code and add ServiceWorkerClientContext.Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,23 @@
 import { h, render } from "preact";
+import "preact/hooks";
 import App from "./App";
 import runtime from "serviceworker-webpack-plugin/lib/runtime";
 import ServiceWorkerClient from "./ServiceWorkerClient";
 import MessageType from "./common/MessageType.enum";
+import ServiceWorkerClientContext from "./ServiceWorkerClient/ServiceWorkerClientContext";
 
 if ("serviceWorker" in navigator) {
   const registration = runtime.register();
 }
 
-const cl = new ServiceWorkerClient()
-cl.request(MessageType.GetItems, "XD").then(console.log)
-
 const appRoot = document.createElement("div");
 appRoot.id = "app";
 document.body.appendChild(appRoot);
 
-render(<App />, appRoot);
+render(
+  // @ts-ignore
+  <ServiceWorkerClientContext.Provider value={new ServiceWorkerClient()}>
+    <App />
+  </ServiceWorkerClientContext.Provider>,
+  appRoot
+);

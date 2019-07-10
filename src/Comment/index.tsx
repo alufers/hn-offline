58861c58677eece51557a5b6adcc881f6a573d31
alufers <alufers@wp.pl>
2019-07-10 12:20:04 +0200

    Added basic styling to the comments

--- src/Comment/index.tsx
+++ src/Comment/index.tsx
@@ -1,20 +1,44 @@
 import ItemWithPopulatedChildren from "../types/ItemWithPopulatedChildren";
 import { Attributes } from "preact";
-import { useMemo } from "preact/hooks";
+import { useMemo, useState } from "preact/hooks";
 import "./style.less";
+import Score from "../Score";
+import timeAgoFromTimestamp from "../util/timeAgoFromTimestamp";
 
 export default function Comment({
   item
 }: { item: ItemWithPopulatedChildren } & Attributes) {
   const htmlData = useMemo(() => ({ __html: item.text }), [item.text]); // memoize the html to aid rendering
+  const [highlightHint, setHighlightHint] = useState(false);
+
   return (
     <div styleName="comment">
-      <strong>{item.by}</strong>
-      <p dangerouslySetInnerHTML={htmlData} />
-      {item.populatedChildren &&
-        item.populatedChildren.map(child => (
-          <Comment key={child.id} item={child} />
-        ))}
+      {highlightHint ? (
+        <div styleName="collapse-line highlight-hint" />
+      ) : (
+        <div styleName="collapse-line" />
+      )}
+      <div styleName="inside">
+        <div styleName="meta">
+          <span styleName="meta-item username">{item.by}</span>
+          <span styleName="meta-item time">
+            {timeAgoFromTimestamp(item.time)}
+          </span>
+          <span
+            onMouseOver={() => setHighlightHint(true)}
+            onMouseOut={() => setHighlightHint(false)}
+            styleName="meta-item collapse-button"
+            title="Collapse"
+          />
+        </div>
+        <p dangerouslySetInnerHTML={htmlData} />
+        <div className="children">
+          {item.populatedChildren &&
+            item.populatedChildren.map(child => (
+              <Comment key={child.id} item={child} />
+            ))}
+        </div>
+      </div>
     </div>
   );
 }

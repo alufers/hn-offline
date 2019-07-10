561b28ac71689a5b5a37d9b21441f637095ad193
alufers <alufers@wp.pl>
2019-07-10 20:28:04 +0200

    Add some basic error handling

--- readability-proxy/index.js
+++ readability-proxy/index.js
@@ -1,15 +1,18 @@
 const fetch = require("node-fetch");
 const JSDOM = require("jsdom").JSDOM;
 const Readability = require("./Readability");
+const url = require("url");
+const { send } = require("micro");
 
 module.exports = async (req, res) => {
-  const resp = await fetch("https://github.com/jsdom/jsdom");
+  const urlToFetch = url.parse(req.url, true).query.url;
+  if (typeof urlToFetch !== "string") {
+    send(res, 400, { error: "Bad URL type (not a string)" });
+    return;
+  }
+  const resp = await fetch(urlToFetch);
   const test = await resp.text();
   const dom = new JSDOM(test);
   const article = new Readability(dom.window.document).parse();
   return article;
-  //   //   return {
-  //   //     content: article.content
-  //   //   };
-  //   res.end(article.content);
 };
